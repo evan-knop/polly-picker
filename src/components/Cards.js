9316d107290f3722d242d69599e8cf6d958b22d5
Evan Knop <eknop827@uwsp.edu>
2020-10-30 23:04:59 -0500

    Two empty cards, outline for app setup

---
+++ src/components/Cards.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./Cards.css";
+
+function Cards() {
+    return (
+        <div className="pollyCards">
+            <div className = "pollyCards__cardContainer">
+                <div className="card">
+
+                </div>  
+                <div className="card">
+
+                
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Cards
