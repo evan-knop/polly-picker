db216d38cb71d2211134a4cb3041db91f21f39b5
Evan Knop <eknop827@uwsp.edu>
2020-10-31 22:41:37 -0500

    Added sample pictures, added placeholder

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import "./Cards.css";
 import Button from '@material-ui/core/Button';
+import Image from '../images/IMG_2969.jpeg';
+import Image2 from '../images/IMG_0203.jpeg';
 
 function Cards() {
     return (
@@ -8,17 +10,17 @@ function Cards() {
             <div className = "pollyCards__cardContainer">
                 
 
-                //Button onClick: 
+                {/* Button onClick: 
                 // 1. Clicked item moved into new "Next Round" collection of photos
-                // 2. Two new pictures to replace the old ones.
+                // 2. Two new pictures to replace the old ones. */}
                 <Button className="leftCard" onClick={() => {alert('leftCard Wins!')}}> 
-                    <div className="card">
+                    <div className="card" style={{ backgroundImage: `url(${Image})`}}>
                         
                     </div>  
                 </Button>
 
                 <Button className="rightCard" onClick={() => {alert('rightCard Wins!')}}>
-                    <div className="card">                
+                    <div className="card" style={{ backgroundImage: `url(${Image2})`}}>             
                     
                     </div>
                 </Button>
