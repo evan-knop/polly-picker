8544ede2f8dd3dc8b151f6ebc4bde76ec8682428
Evan Knop <eknop827@uwsp.edu>
2020-10-31 20:41:39 -0500

    OnClick function setup - needs functionality

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import "./Cards.css";
+import Button from '@material-ui/core/Button';
 
 function Cards() {
     return (
         <div className="pollyCards">
             <div className = "pollyCards__cardContainer">
-                <div className="card">
-
-                </div>  
-                <div className="card">
-
                 
-                </div>
+
+                //Button onClick: 
+                // 1. Clicked item moved into new "Next Round" collection of photos
+                // 2. Two new pictures to replace the old ones.
+                <Button className="leftCard" onClick={() => {alert('leftCard Wins!')}}> 
+                    <div className="card">
+                        
+                    </div>  
+                </Button>
+
+                <Button className="rightCard" onClick={() => {alert('rightCard Wins!')}}>
+                    <div className="card">                
+                    
+                    </div>
+                </Button>
             </div>
         </div>
     )
