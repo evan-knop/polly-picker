6b5d5fe162500f793125dc6dc13430cd26d23f89
Evan Knop <eknop827@uwsp.edu>
2020-11-03 16:04:59 -0600

    Added all images in

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,7 +2,16 @@ import React, {useState, useEffect} from 'react';
 import "./Cards.css";
 import Button from '@material-ui/core/Button';
 import axios from '../axios';
-import img1 from '../images/IMG_0039.jpeg';
+import img1 from '../images/60225547512__DC211E24-228F-44FA-9917-EAE11856F8FC.jpeg';
+import img2 from '../images/IMG_0039.jpeg';
+import img3 from '../images/IMG_0128.jpeg';
+import img4 from '../images/IMG_0203.jpeg';
+import img5 from '../images/IMG_0268.jpeg';
+import img6 from '../images/IMG_0510.jpeg';
+import img7 from '../images/IMG_2969.jpeg';
+import img8 from '../images/IMG_2988.jpeg';
+
+
 
 function Cards() {
 
@@ -11,15 +20,15 @@ function Cards() {
     // Runs this once, not again. THIS IS A HOOK
     // Async function inside useEffect 
 
-    useEffect(() => {
-        async function fetchData() {
-            const req = await axios.get('/polly/cards');
+    // useEffect(() => {
+    //     async function fetchData() {
+    //         const req = await axios.get('/polly/cards');
 
-            setImages(req.data);
-        }
+    //         setImages(req.data);
+    //     }
 
-        fetchData();
-    }, []);
+    //     fetchData();
+    // }, []);
 
     return (
         <div className="pollyCards">
@@ -35,9 +44,9 @@ function Cards() {
 
                 db.mycoll.aggregate([{ $sample: { size: 1 } }])
                 */}
-                {nextRoundImgs.map((imgs) =>(
+                {/* {nextRoundImgs.map((imgs) =>( */}
 
-                <Button className="leftCard" key={imgs._id} onClick={() => {console.log(imgs.path)}}> 
+                <Button className="leftCard" onClick={() => {console.log('')}}> 
                     <div className="card"  style={{ backgroundImage: `url(${img1})` }}>
                         
 
@@ -45,7 +54,7 @@ function Cards() {
 
                     </div>  
                 </Button>
-                ))}
+                {/* ))} */}
                 {/* {nextRoundImgs.map((imgs) =>(
 
                 <Button className="rightCard" onClick={() => {console.log(imgs); alert('rightCard Wins!')}}>
